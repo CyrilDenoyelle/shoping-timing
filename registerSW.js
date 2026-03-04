2679fd91ddb189760b2d06512ce1a482c6935363
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2026-03-04 20:51:32 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ShopingTiming/sw.js', { scope: '/ShopingTiming/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shoping-timing/sw.js', { scope: '/shoping-timing/' })})}
